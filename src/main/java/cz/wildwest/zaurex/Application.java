ea0278ebbd72489219b8aa173ed37bc1964c04ba
Ondřej Kozel <ondrakozel@outlook.com>
2022-03-19 22:04:23 +0100

    Změna fontu

--- src/main/java/cz/wildwest/zaurex/Application.java
+++ src/main/java/cz/wildwest/zaurex/Application.java
@@ -17,7 +17,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
  *
  */
 @SpringBootApplication
-@NpmPackage(value = "@fontsource/lato", version = "4.5.0")
+@NpmPackage(value = "@fontsource/open-sans", version = "4.5.0")
 @Theme(value = "zaurex")
 @PWA(name = "Zaurex", shortName = "Zaurex", offlineResources = {"images/logo.png"})
 @Push
