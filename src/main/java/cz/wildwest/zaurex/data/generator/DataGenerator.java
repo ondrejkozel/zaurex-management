94f618232b58c93797db7d0c1a2056e295ef9c30
Ondřej Kozel <ondrakozel@outlook.com>
2022-03-20 09:41:09 +0100

    Úprava třídy user

--- src/main/java/cz/wildwest/zaurex/data/generator/DataGenerator.java
+++ src/main/java/cz/wildwest/zaurex/data/generator/DataGenerator.java
@@ -23,7 +23,6 @@ public class DataGenerator {
                 logger.info("Using existing database");
                 return;
             }
-            int seed = 123;
 
             logger.info("Generating demo data");
 
@@ -32,16 +31,12 @@ public class DataGenerator {
             user.setName("John Normal");
             user.setUsername("user");
             user.setHashedPassword(passwordEncoder.encode("user"));
-            user.setProfilePictureUrl(
-                    "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80");
             user.setRoles(Collections.singleton(Role.USER));
             userRepository.save(user);
             User admin = new User();
             admin.setName("Emma Powerful");
             admin.setUsername("admin");
             admin.setHashedPassword(passwordEncoder.encode("admin"));
-            admin.setProfilePictureUrl(
-                    "https://images.unsplash.com/photo-1607746882042-944635dfe10e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=128&h=128&q=80");
             admin.setRoles(Set.of(Role.USER, Role.ADMIN));
             userRepository.save(admin);
 
