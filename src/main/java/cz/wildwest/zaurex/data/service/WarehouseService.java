d6f81b5fb9b50cf15fbdf94c8a9012bc6e709390
Ondřej Kozel <ondrakozel@outlook.com>
2022-03-21 16:35:04 +0100

    Vytvoření metody findAllSellable

--- src/main/java/cz/wildwest/zaurex/data/service/WarehouseService.java
+++ src/main/java/cz/wildwest/zaurex/data/service/WarehouseService.java
@@ -4,10 +4,16 @@ import cz.wildwest.zaurex.data.entity.WarehouseItem;
 import cz.wildwest.zaurex.data.service.repository.WarehouseItemRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class WarehouseService extends GenericService<WarehouseItem, WarehouseItemRepository> {
 
     public WarehouseService(WarehouseItemRepository mainRepository) {
         super(mainRepository);
+    }
+
+    public List<WarehouseItem> findAllSellable() {
+        return mainRepository.findAllBySellableEquals(true);
     }
 }
--- src/main/java/cz/wildwest/zaurex/data/service/repository/WarehouseItemRepository.java
+++ src/main/java/cz/wildwest/zaurex/data/service/repository/WarehouseItemRepository.java
@@ -2,5 +2,8 @@ package cz.wildwest.zaurex.data.service.repository;
 
 import cz.wildwest.zaurex.data.entity.WarehouseItem;
 
+import java.util.List;
+
 public interface WarehouseItemRepository extends GenericRepository<WarehouseItem> {
+    List<WarehouseItem> findAllBySellableEquals(boolean sellable);
 }
