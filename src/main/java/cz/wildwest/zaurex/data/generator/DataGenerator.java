c99b0b42b6894845ff9463d631f4a34cec92279d
Ondřej Kozel <ondrakozel@outlook.com>
2022-03-26 18:39:56 +0100

    Lepší naformátování DataGenerator

--- src/main/java/cz/wildwest/zaurex/data/generator/DataGenerator.java
+++ src/main/java/cz/wildwest/zaurex/data/generator/DataGenerator.java
@@ -34,57 +34,66 @@ public class DataGenerator {
             //
             logger.info("Generating demo data");
             //
-            logger.info("... generating 5 User entities...");
-            User salesman = new User();
-            salesman.setName("Prodavač");
-            salesman.setUsername("prodavac");
-            salesman.setHashedPassword(passwordEncoder.encode("prodavac"));
-            salesman.setRoles(Collections.singleton(Role.SALESMAN));
-            //
-            User warehouseman = new User();
-            warehouseman.setName("Skladník");
-            warehouseman.setUsername("skladnik");
-            warehouseman.setHashedPassword(passwordEncoder.encode("skladnik"));
-            warehouseman.setRoles(Collections.singleton(Role.WAREHOUSEMAN));
-            //
-            User shiftLeader = new User();
-            shiftLeader.setName("Vedoucí směny");
-            shiftLeader.setUsername("vedoucismeny");
-            shiftLeader.setHashedPassword(passwordEncoder.encode("vedoucismeny"));
-            shiftLeader.setRoles(Collections.singleton(Role.SHIFT_LEADER));
-            //
-            User manager = new User();
-            manager.setName("Manažer");
-            manager.setUsername("manazer");
-            manager.setHashedPassword(passwordEncoder.encode("manazer"));
-            manager.setRoles(Set.of(Role.values()));
-            //
-            User shiftLeader2 = new User();
-            shiftLeader2.setName("Vedocí směny prodavač");
-            shiftLeader2.setUsername("vedoucismeny2");
-            shiftLeader2.setHashedPassword(passwordEncoder.encode("vedoucismeny2"));
-            shiftLeader2.setRoles(Set.of(Role.SHIFT_LEADER, Role.SALESMAN));
-            //
-            userRepository.saveAll(List.of(salesman, warehouseman, shiftLeader, manager, shiftLeader2));
+            createUsers(passwordEncoder, userRepository);
+            createWarehouseItems(warehouseService, warehouseItemVariantService);
+            createHolidays(userRepository, holidayService);
             //
             logger.info("Generated demo data");
-            //
-            WarehouseItem bunda_tilak = new WarehouseItem("Bunda Tilak", "Zimní bunda Tilak vás zahřeje v každém ročním období!");
-            bunda_tilak.setCategory(WarehouseItem.Category.HIKING);
-            warehouseService.save(bunda_tilak);
-            warehouseItemVariantService.saveAll(Set.of(
-                    new WarehouseItem.Variant(bunda_tilak, "zelená", 84, 499.9),
-                    new WarehouseItem.Variant(bunda_tilak, "černá", 10, 514.9)
-            ));
-            //
-            Holiday holiday = new Holiday(userRepository.findByUsername("skladnik"), LocalDate.now(), LocalDate.now().plusWeeks(1));
-            holiday.setUserMessage("Chci jet k moři.");
-            Holiday holiday2 = new Holiday(userRepository.findByUsername("skladnik"), LocalDate.now().plusMonths(1), LocalDate.now().plusMonths(1).plusWeeks(1));
-            holiday2.setUserMessage("Prosím pustťe mě \uD83D\uDE1E");
-            holiday2.setStatus(Holiday.Status.APPROVED);
-            holiday2.setManagerResponse("Tak jo :)");
-            holidayService.saveAll(List.of(holiday, holiday2));
         };
+    }
+
+    private void createUsers(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+        User salesman = new User();
+        salesman.setName("Prodavač");
+        salesman.setUsername("prodavac");
+        salesman.setHashedPassword(passwordEncoder.encode("prodavac"));
+        salesman.setRoles(Collections.singleton(Role.SALESMAN));
+        //
+        User warehouseman = new User();
+        warehouseman.setName("Skladník");
+        warehouseman.setUsername("skladnik");
+        warehouseman.setHashedPassword(passwordEncoder.encode("skladnik"));
+        warehouseman.setRoles(Collections.singleton(Role.WAREHOUSEMAN));
+        //
+        User shiftLeader = new User();
+        shiftLeader.setName("Vedoucí směny");
+        shiftLeader.setUsername("vedoucismeny");
+        shiftLeader.setHashedPassword(passwordEncoder.encode("vedoucismeny"));
+        shiftLeader.setRoles(Collections.singleton(Role.SHIFT_LEADER));
+        //
+        User manager = new User();
+        manager.setName("Manažer");
+        manager.setUsername("manazer");
+        manager.setHashedPassword(passwordEncoder.encode("manazer"));
+        manager.setRoles(Set.of(Role.values()));
+        //
+        User shiftLeader2 = new User();
+        shiftLeader2.setName("Vedocí směny prodavač");
+        shiftLeader2.setUsername("vedoucismeny2");
+        shiftLeader2.setHashedPassword(passwordEncoder.encode("vedoucismeny2"));
+        shiftLeader2.setRoles(Set.of(Role.SHIFT_LEADER, Role.SALESMAN));
+        //
+        userRepository.saveAll(List.of(salesman, warehouseman, shiftLeader, manager, shiftLeader2));
+    }
+
+    private void createWarehouseItems(WarehouseService warehouseService, WarehouseItemVariantService warehouseItemVariantService) {
+        WarehouseItem bunda_tilak = new WarehouseItem("Bunda Tilak", "Zimní bunda Tilak vás zahřeje v každém ročním období!");
+        bunda_tilak.setCategory(WarehouseItem.Category.HIKING);
+        warehouseService.save(bunda_tilak);
+        warehouseItemVariantService.saveAll(Set.of(
+                new WarehouseItem.Variant(bunda_tilak, "zelená", 84, 499.9),
+                new WarehouseItem.Variant(bunda_tilak, "černá", 10, 514.9)
+        ));
+    }
+
+    private void createHolidays(UserRepository userRepository, HolidayService holidayService) {
+        Holiday holiday = new Holiday(userRepository.findByUsername("skladnik"), LocalDate.now(), LocalDate.now().plusWeeks(1));
+        holiday.setUserMessage("Chci jet k moři.");
+        Holiday holiday2 = new Holiday(userRepository.findByUsername("skladnik"), LocalDate.now().plusMonths(1), LocalDate.now().plusMonths(1).plusWeeks(1));
+        holiday2.setUserMessage("Prosím pustťe mě \uD83D\uDE1E");
+        holiday2.setStatus(Holiday.Status.APPROVED);
+        holiday2.setManagerResponse("Tak jo :)");
+        holidayService.saveAll(List.of(holiday, holiday2));
     }
 
 }
