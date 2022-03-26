5b763c65987a38f651b5d9c7072b55ac6b265799
Ondřej Kozel <ondrakozel@outlook.com>
2022-03-26 18:32:55 +0100

    Ukázka tvorby nápovědy

--- src/main/java/cz/wildwest/zaurex/help/Helpers.java
+++ src/main/java/cz/wildwest/zaurex/help/Helpers.java
@@ -1,6 +1,7 @@
 package cz.wildwest.zaurex.help;
 
 import cz.wildwest.zaurex.views.homePage.HomePageView;
+import cz.wildwest.zaurex.views.warehouse.WarehouseView;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -17,15 +18,21 @@ public class Helpers {
     }
 
     private static void buildHelpers() {
+        //jak tvořit nápovědu:
         HELPER_MAP.put(HomePageView.class, new Helper(
-                "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vestibulum fermentum tortor id mi." +
+                "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Vestibulum fermentum tortor id mi. " +
                         "Maecenas fermentum, sem in pharetra pellentesque, velit turpis volutpat ante, in pharetra metus odio a lectus.",
-                "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos." +
+                "Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. " +
                         "Vivamus ac leo pretium faucibus. Sed convallis magna eu sem." +
-                        "Integer malesuada. Mauris elementum mauris vitae tortor. Sed convallis magna eu sem. Etiam neque." +
-                        "Aenean fermentum risus id tortor. Mauris dictum facilisis augue. Et harum quidem rerum facilis est et expedita distinctio." +
-                        "Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante." +
+                        "Integer malesuada. Mauris elementum mauris vitae tortor. Sed convallis magna eu sem. Etiam neque. " +
+                        "Aenean fermentum risus id tortor. Mauris dictum facilisis augue. Et harum quidem rerum facilis est et expedita distinctio. " +
+                        "Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. " +
                         "Sed elit dui, pellentesque a, faucibus vel, interdum nec, diam. Etiam dictum tincidunt diam."
+        ));
+        HELPER_MAP.put(WarehouseView.class, new Helper(
+                "Nápověda skladu, blah blah",
+                //dlouhý text klidně může zůstat prázdný:
+                ""
         ));
     }
 
