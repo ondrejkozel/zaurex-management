8da2fbfefc643ee89331101cf65fc237106d96c1
Marek <mapribyl@seznam.cz>
2022-03-26 16:51:33 +0100

    Úprava stánky informací

--- src/main/java/cz/wildwest/zaurex/views/informations/Informations.java
+++ src/main/java/cz/wildwest/zaurex/views/informations/Informations.java
@@ -27,15 +27,24 @@ public class Informations extends VerticalLayout {
         add(new H3("Obecný popis"));
         add(new Paragraph("Zde můžete zjistit, jaké věci se aktuálně nachází na skladě. Mohou se zde přidávat i odstraňovat zboží a jeho varianty. "));
         //        
-        add(new H3("Obecný popis"));
+        add(new H3("Přístup"));
         add(new Paragraph("Plný přístup sem má pouze manažer. Pokladník se sem může podívat kolik je kterého produktu, ale nemůže tu nic upravovat."));
-        //
-                
+        //     
         add(new H3("Ovládací prvky"));
         add(new H4("Přidávání nového zboží"));
         add(new Paragraph("Nové zboží se dá přidat tlačítkem v pravém dolním roku obrazovky Nové zboží, anebo klávesovou zkratkou alt + N. U nového zboží je potřeba nastavit název a vybrat, do jaké kategorie zboží patří. Nepovinné pole je krátký popis, kde můžete výrobek více specifikovat. Pokud rovnou chcete přidat variantu, rozklikněte \"Varianty\" a stiskněte přidat variantu. Každá varianta potřebuje mít název (barvu), cenu a množství (může být i 0). Nepovinné je napsání velikosti nebo nějaké jiné poznámky, která specifikuje danou variantu."));
         add(new H4("Upravování již existujícího zboží"));
+        add(new Paragraph("Pokud chcete upravit již existující zboží, stačí na něj jednoduše kliknout. Můžete tam upravovat nejen zboží samotné, ale i jeho varynty. Ty se zde dají nejen upravovat, ale i pridávat (opět stiskutím přidat variantu), anebo odebrat variantu (stsknutím červeného křížku napravo od varianty). Všechny změny musejí být uloženy tlačítkem Uložit, jinak nebudou aplikovány."));
+        add(new H4("Odstraňování zboží"));
+        add(new Paragraph("Doplním až mit to zase pojede"));
         //
+        //
+        add(new H2("Dovolené"));
+        //        
+        add(new H3("Obecný popis"));
+        add(new Paragraph("Dopním až mi to zase bude fungovat, moc se omlouvám, že jsou se mnou takové komplikace"));
+        //        
+        
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
