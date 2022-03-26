f1ca13609c3aa0825235afeef4c9a429fecc1e5f
Ondřej Kozel <ondrakozel@outlook.com>
2022-03-26 16:57:36 +0100

    Nastavení cesty

--- src/main/java/cz/wildwest/zaurex/views/informations/Informations.java
+++ src/main/java/cz/wildwest/zaurex/views/informations/Informations.java
@@ -12,7 +12,7 @@ import cz.wildwest.zaurex.views.MainLayout;
 import javax.annotation.security.PermitAll;
 
 @PageTitle("Informace k používání")
-@Route(value = "", layout = MainLayout.class)
+@Route(value = "informations", layout = MainLayout.class)
 @PermitAll
 public class Informations extends VerticalLayout {
 
