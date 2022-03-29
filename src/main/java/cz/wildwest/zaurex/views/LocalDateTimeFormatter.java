44f6531d25ee1361835214bbda0eacd16c684439
Ondřej Kozel <ondrakozel@outlook.com>
2022-03-29 16:27:31 +0200

    Vytvoření českého DateTimeFormatteru
    solves #21

---
+++ src/main/java/cz/wildwest/zaurex/views/LocalDateTimeFormatter.java
@@ -0,0 +1,23 @@
+package cz.wildwest.zaurex.views;
+
+import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.util.Locale;
+
+public class LocalDateTimeFormatter {
+    private LocalDateTimeFormatter() {}
+
+    public static final Locale LOCALE = new Locale("cs", "CZ");
+
+    public static DateTimeFormatter ofLongDate() {
+        return DateTimeFormatter.ofLocalizedDate(FormatStyle.LONG).localizedBy(LOCALE);
+    }
+
+    public static DateTimeFormatter ofFullDate() {
+        return DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).localizedBy(LOCALE);
+    }
+
+    public static DateTimeFormatter ofShortDateTime() {
+        return DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT).localizedBy(LOCALE);
+    }
+}
