e1ed8a56f103e36d7a7c8f0f7b466be2f70c92d0
Marek <mapribyl@seznam.cz>
2022-03-29 09:34:39 +0200

    Vytvoření O aplikaci

---
+++ src/main/java/cz/wildwest/zaurex/views/aboutApp/AboutAppView.java
@@ -0,0 +1,34 @@
+package cz.wildwest.zaurex.views.aboutApp;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.FlexComponent;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import cz.wildwest.zaurex.views.MainLayout;
+import javax.annotation.security.PermitAll;
+
+@PageTitle("O aplikaci")
+@Route(value = "", layout = MainLayout.class)
+@PermitAll
+public class AboutAppView extends VerticalLayout {
+
+    public AboutAppView() {
+        setSpacing(false);
+        //
+        Image zaurex = new Image("images/logo-full.png", "Zaurex");
+        zaurex.addClassNames("home-page-logo");
+        add(zaurex);
+        //
+        add(new H2("Vítejte v Zaurex management"));
+        add(new Paragraph("Lorem ipsum dolor sit amet"));
+        //
+        setSizeFull();
+        setJustifyContentMode(FlexComponent.JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(FlexComponent.Alignment.CENTER);
+        getStyle().set("text-align", "left");
+    }
+
+}
