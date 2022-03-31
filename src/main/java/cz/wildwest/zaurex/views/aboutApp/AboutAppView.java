89db47395401530c29d394c2673010c6ad9f4990
Marek <mapribyl@seznam.cz>
2022-03-31 18:58:15 +0200

    Šestá verze stejného textu

--- src/main/java/cz/wildwest/zaurex/views/aboutApp/AboutAppView.java
+++ src/main/java/cz/wildwest/zaurex/views/aboutApp/AboutAppView.java
@@ -20,7 +20,7 @@ public class AboutAppView extends VerticalLayout {
                 "<H2>O aplikaci</H2>"
                 + "<p>Aplikace byla vytvořena do nultého ročníku soutěže Tour de App. Aplikaci vytvořil tým <b>Brněnský divoký západ</b> ze školy Gymnázium Brno, třída Kapitána Jaroše. Členy týmu jsou studenti třetího ročníku čtyletého studia <b>Ondřej Kozel</b>, který se zasloužil o funkčnost aplikace a <b>Marek Přibyl</b>, který mu nosil pití.</p>"
                 + "<H4>Uživatelská příručka</H4>"
-                + "Na každé stránce se nachází jednoduché vysvětlení, co se na dané straně nachází a jak se obsluhuje. Toto vysvětlení se dá spustit buď kliknutím na ikonku otazníku v pravém horním rohu, anebo klávesovou zkratkou <code>Alt&nbsp+&nbspH</code>. Stránky, na které daná osoba nemá přístup, se jí ani v nabídce nezobrazují."
+                + "Na každé stránce se nachází jednoduché vysvětlení, co se na dané straně nachází a jak se obsluhuje. Toto vysvětlení se dá spustit buď kliknutím na ikonku otazníku v pravém horním rohu, anebo klávesovou zkratkou <code>Alt&nbsp;+&nbsp;H</code>. Stránky, na které daná osoba nemá přístup, se jí v nabídce nezobrazují."
                 + "<H4>Technické požadavky</H4>"
                 + "<p>Aplikace je navržená tak, aby měla minimální požadavky na internetové připojení. Systémové požadavy pro počítač jsou alespoň Windows Vista SP2 nebo Mac OS X 10.8.3. Minimální potřebná velikost obrazovky je 300&nbsp;x&nbsp;300&nbsp;pixelů.</p>"
                 + "<H4>Kontaktní údaje</H4>"
